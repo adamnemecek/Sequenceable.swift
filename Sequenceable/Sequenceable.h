7b3bd4d4fb82b360556241e6e11a2b9f350a9a55
Adam Nemecek <adamnemecek>
2016-11-10 19:05:23 -0800

    Initial Commit

---
+++ Sequenceable/Sequenceable.h
@@ -0,0 +1,19 @@
+//
+//  Sequenceable.h
+//  Sequenceable
+//
+//  Created by Adam Nemecek on 11/10/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sequenceable.
+FOUNDATION_EXPORT double SequenceableVersionNumber;
+
+//! Project version string for Sequenceable.
+FOUNDATION_EXPORT const unsigned char SequenceableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sequenceable/PublicHeader.h>
+
+
